6156ca2f71e547e030b07ef2b5c1a8fa1ed71de4
rabihrajaa <rajaarabihh@gmail.com>
2025-05-15 23:29:33 +0100

    search_customers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {provideHttpClient} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideHttpClient(),
+    ReactiveFormsModule
   ]
 };
