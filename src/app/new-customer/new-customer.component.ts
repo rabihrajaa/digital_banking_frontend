daceaffebbb498459a9f87339ce10b0d3f44757f
rabihrajaa <rajaarabihh@gmail.com>
2025-05-16 01:37:16 +0100

    new_customer

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-new-customer',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './new-customer.component.html',
   styleUrl: './new-customer.component.css'
 })
-export class NewCustomerComponent {
+export class NewCustomerComponent implements OnInit {
+    newCustomerFormGroup!: FormGroup;
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+    this.newCustomerFormGroup = this.fb.group({
+      name: this.fb.control(null),
+      email: this.fb.control(null),
+    })
+    }
 
 }
