a35c4050d724a990ab4d3f60988af076dd82035d
rabihrajaa <rajaarabihh@gmail.com>
2025-05-17 23:06:39 +0100

    Accounts_operations

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AccountsService} from '../services/accounts.service';
 import {AccountDetails} from '../model/accounts.model';
-import {Observable} from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
 import {AsyncPipe, DatePipe, DecimalPipe, NgClass, NgIf} from '@angular/common';
 
 @Component({
@@ -19,40 +19,82 @@ import {AsyncPipe, DatePipe, DecimalPipe, NgClass, NgIf} from '@angular/common';
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent implements OnInit {
-    accountFormGroup!: FormGroup;
-    currentPage: number=0;
-    pageSize: number = 5;
-    accountObservable!: Observable<AccountDetails>;
-    operationFormGroup!: FormGroup;
+  accountFormGroup! : FormGroup;
+  currentPage : number =0;
+  pageSize : number =5;
+  accountObservable! : Observable<AccountDetails>
+  operationFromGroup! : FormGroup;
+  errorMessage! :string ;
 
-    constructor(private fb: FormBuilder,private accountService: AccountsService) {
-    }
+  constructor(private fb : FormBuilder, private accountService : AccountsService) { }
 
   ngOnInit(): void {
-    this.accountFormGroup = this.fb.group({
-      accountId: ['', Validators.required],
+    this.accountFormGroup=this.fb.group({
+      accountId : this.fb.control('')
     });
-
-    this.operationFormGroup = this.fb.group({
-      operationType:this.fb.control(null, [Validators.required]),
-      amount: this.fb.control(null, [Validators.required]),
-      description: this.fb.control(null, [Validators.required]),
-      accountDescription: this.fb.control(null, [Validators.required])
-    })
-
-    }
+    this.operationFromGroup=this.fb.group({
+      operationType : this.fb.control(null),
+      amount : this.fb.control(0),
+      description : this.fb.control(null),
+      accountDestination : this.fb.control(null)
+    })}
 
   handleSearchAccount() {
-      let accountId: string = this.accountFormGroup.value.accountId;
-    this.accountObservable=this.accountService.getAccount(accountId,this.currentPage,this.pageSize)
+    let accountId : string =this.accountFormGroup.value.accountId;
+    this.accountObservable=this.accountService.getAccount(accountId,this.currentPage, this.pageSize).pipe(
+      catchError(err => {
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
   }
 
-  gotoPage(page:number) {
-    this.currentPage = page;
-    this.handleSearchAccount()
+  gotoPage(page: number) {
+    this.currentPage=page;
+    this.handleSearchAccount();
   }
 
   handleAccountOperation() {
+    let accountId :string = this.accountFormGroup.value.accountId;
+    let operationType=this.operationFromGroup.value.operationType;
+    let amount :number =this.operationFromGroup.value.amount;
+    let description :string =this.operationFromGroup.value.description;
+    let accountDestination :string =this.operationFromGroup.value.accountDestination;
+    if(operationType=='DEBIT'){
+      this.accountService.debit(accountId, amount,description).subscribe({
+        next : (data)=>{
+          alert("Success Credit");
+          this.operationFromGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err)=>{
+          console.log(err);
+        }
+      });
+    } else if(operationType=='CREDIT'){
+      this.accountService.credit(accountId, amount,description).subscribe({
+        next : (data)=>{
+          alert("Success Debit");
+          this.operationFromGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err)=>{
+          console.log(err);
+        }
+      });
+    }
+    else if(operationType=='TRANSFER'){
+      this.accountService.transfer(accountId,accountDestination, amount,description).subscribe({
+        next : (data)=>{
+          alert("Success Transfer");
+          this.operationFromGroup.reset();
+          this.handleSearchAccount();
+        },
+        error : (err)=>{
+          console.log(err);
+        }
+      });
 
+    }
   }
 }
--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -14,5 +14,19 @@ export class AccountsService {
     return this.http.get<AccountDetails>(this.backendHost+"/accounts/"+accountId+"pageOperations?page="+page+"&size="+size);
   }
 
+  public debit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(this.backendHost+"/accounts/debit",data);
+  }
+
+  public credit(accountId : string, amount : number, description:string){
+    let data={accountId : accountId, amount : amount, description : description}
+    return this.http.post(this.backendHost+"/accounts/credit",data);
+  }
+  public transfer(accountSource: string,accountDestination: string, amount : number, description:string){
+    let data={accountSource, accountDestination, amount, description }
+    return this.http.post(this.backendHost+"/accounts/transfer",data);
+  }
+
 
 }
